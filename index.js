b77b550f1f5f809ffda997bef493480c7c15836c
reza <rezarezaeijamiuk@gmail.com>
2022-06-07 15:38:19 +0100

    router imported in in main index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const config = require("config");
 const debug = require("debug");
 
+const router = require("./src/routes");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());
@@ -13,6 +15,8 @@ mongoose
   .connect(config.get("db.address"))
   .then(() => console.log("Connected to the mongoDB"))
   .catch((err) => console.log("Could not connect to the mongoDB!"));
+
+app.use("/api", router);
 
 const port = process.env.PORT || 3000;
 app.listen(port, console.log(`listening on port ${port}`));
