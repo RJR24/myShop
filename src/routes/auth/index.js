adfafce5b1a0aa8101eb24f23fd66f9e1fac94b4
reza <rezarezaeijamiuk@gmail.com>
2022-06-07 18:26:49 +0100

    register/login validator required and used in index.js

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("./controller");
+const validator = require("./validator");
 
-router.post("/register", controller.register);
-router.post("/login", controller.login);
+router.post("/register", validator.registerValidator(), controller.register);
+router.post("/login", validator.loginValidator(), controller.login);
 
 module.exports = router;
