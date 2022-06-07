20a0a53fc99c21f335bef956e15527bc2bc055e1
reza <rezarezaeijamiuk@gmail.com>
2022-06-07 16:26:14 +0100

    process.env set to 5000 & console.log replaced with debug in main index.js

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ app.use(express.json());
 
 mongoose
   .connect(config.get("db.address"))
-  .then(() => console.log("Connected to the mongoDB"))
-  .catch((err) => console.log("Could not connect to the mongoDB!"));
+  .then(() => debug("Connected to the mongoDB"))
+  .catch((err) => debug("Could not connect to the mongoDB!"));
 
 app.use("/api", router);
 
 const port = process.env.PORT || 3000;
-app.listen(port, console.log(`listening on port ${port}`));
+app.listen(port, debug(`listening on port ${port}`));
