e5e47169372d1ddfa6506429b8457b82902e16f1
reza <rezarezaeijamiuk@gmail.com>
2022-06-07 15:19:43 +0100

    connected to the mongoDB database

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ const debug = require("debug");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());
+
+mongoose
+  .connect(config.get("db.address"))
+  .then(() => console.log("Connected to the mongoDB"))
+  .catch((err) => console.log("Could not connect to the mongoDB!"));
+
+const port = process.env.PORT || 3000;
+app.listen(port, console.log(`listening on port ${port}`));
