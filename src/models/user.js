3ae25c963f263e7c770ef941e51bb96cf74060a5
reza <rezarezaeijamiuk@gmail.com>
2022-06-08 11:39:37 +0100

    mongoose time stamp added to the user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
+const timeStamp = require("mongoose-timestamp");
 
-const User = mongoose.model(
-  'User',
-  new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    name: { type: String, required: true },
-    password: { type: String, required: true },
-    isAdmin: { type: Boolean, default: false },
-  })
-);
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
+});
+userSchema.plugin(timeStamp);
 
+const User = mongoose.model("User", userSchema);
 module.exports = User;
