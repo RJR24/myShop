2f8c4d2ec187b2753d7433a540d7edbc4fb9ceb7
reza <rezarezaeijamiuk@gmail.com>
2022-06-08 10:48:22 +0100

    validate middleware & error handeling for the validation done

--- src/routes/controller.js
+++ src/routes/controller.js
@@ -1,7 +1,27 @@
 const autoBind = require("auto-bind");
+const { validationResult } = require("express-validator");
 
 module.exports = class {
   constructor() {
     autoBind(this);
+  }
+
+  validationBody(req, res) {
+    const result = validationResult(req);
+    if (!result.isEmpty) {
+      const errors = result.array();
+      const messages = [];
+      errors.forEach((err) => messages.push(err.msg));
+      res.status(400).json({
+        message: "validation error!",
+        data: messages,
+      });
+      return false;
+    }
+    return true;
+  }
+  validate(req, res, next) {
+    if (!validationBody(req, res)) return;
+    next();
   }
 };
