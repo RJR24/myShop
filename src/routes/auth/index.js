680ca995b6e70f34573515b48187543f1d72ad76
reza <rezarezaeijamiuk@gmail.com>
2022-06-08 10:48:54 +0100

    validate middleware added to the login and register api

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -3,7 +3,17 @@ const router = express.Router();
 const controller = require("./controller");
 const validator = require("./validator");
 
-router.post("/register", validator.registerValidator(), controller.register);
-router.post("/login", validator.loginValidator(), controller.login);
+router.post(
+  "/register",
+  validator.registerValidator(),
+  controller.validate,
+  controller.register
+);
+router.post(
+  "/login",
+  validator.loginValidator(),
+  controller.validate,
+  controller.login
+);
 
 module.exports = router;
