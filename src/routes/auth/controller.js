62d7fcb562c38b90c57007a1ac2ff63176a37063
reza <rezarezaeijamiuk@gmail.com>
2022-06-08 12:44:51 +0100

    bcrypt used for password hashing, user registration completed

--- src/routes/auth/controller.js
+++ src/routes/auth/controller.js
@@ -1,8 +1,9 @@
 const controller = require("./../controller");
 const _ = require("lodash");
+const bcrypt = require("bcrypt");
 
 module.exports = new (class extends controller {
-  async register(req, res) {
+  async register(req, res) { 
     let user = await this.User.findOne({ email: req.body.email });
     if (user) {
       return this.response({
@@ -12,6 +13,15 @@ module.exports = new (class extends controller {
       });
     }
     user = new this.User(_.pick(req.body, ["email", "name", "password"]));
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    await user.save();
+    this.response({
+      res,
+      message: "you have successfully registered",
+      data: _.pick(user, ["_id", "name", "email"]),
+    });
   }
 
   async login(req, res) {
