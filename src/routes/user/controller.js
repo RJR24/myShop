13e635a52957bba9f482a68d8a2d8ae1a0c94097
reza <rezarezaeijamiuk@gmail.com>
2022-06-08 15:16:13 +0100

    user controller created, dashboard/me methods started

---
+++ src/routes/user/controller.js
@@ -0,0 +1,15 @@
+const controller = require("../controller");
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = new (class extends controller {
+  async dashboard(req, res) {
+
+  }
+
+  async me(req, res) {
+    
+  }
+})();
