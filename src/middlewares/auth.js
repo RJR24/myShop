5b5bb059d88d21a3a6071645c5fb2ea3b402a07e
reza <rezarezaeijamiuk@gmail.com>
2022-06-09 13:23:21 +0100

    login authentication middleware created

---
+++ src/middlewares/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const User = require("../models/user");
+
+async function isLoggedIn(req, res, next) {
+  const token = req.header("x-auth-token");
+  if (!token) res.status(401).send("access denied");
+  try {
+    const decoded = jwt.verify(token, config.get("jwt_key"));
+    const user = await User.findById(decoded._id);
+    console.log(user);
+    req.user = user;
+    next();
+  } catch (ex) {
+    res.status(400).send("invalid token");
+  }
+}
+
+module.exports = isLoggedIn;
