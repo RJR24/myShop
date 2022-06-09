4a4f8b0d9a05c2949274fb207a616f44fe68787a
reza <rezarezaeijamiuk@gmail.com>
2022-06-09 13:46:45 +0100

    admin router added

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const authRouter = require("./auth");
 const userRouter = require("./user");
+const adminRouter = require("./admin");
 const isLoggedIn = require("../middlewares/auth");
 
 router.use("/auth", authRouter);
 router.use("/user", isLoggedIn, userRouter);
+router.use("/admin", isLoggedIn, adminRouter);
 
 module.exports = router;
