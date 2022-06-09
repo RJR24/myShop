e95fa220e831ceac3457e659b9c14112ffe3d740
reza <rezarezaeijamiuk@gmail.com>
2022-06-09 13:35:35 +0100

    working on user controller (dashboard/me)

--- src/routes/user/controller.js
+++ src/routes/user/controller.js
@@ -1,15 +1,13 @@
 const controller = require("../controller");
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const config = require("config");
 
 module.exports = new (class extends controller {
   async dashboard(req, res) {
-
+    res.send("user dashboard");
   }
 
   async me(req, res) {
-    
+    this.response({ res, data: _.pick(req.user, ["name", "email"]) });
   }
 })();
