03b03542fca269ce7e1f9e2e7c85ce48f99dea8c
reza <rezarezaeijamiuk@gmail.com>
2022-06-09 13:24:23 +0100

    isLoggedIn middleware reuired and used in user router

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const authRouter = require("./auth");
 const userRouter = require("./user");
+const isLoggedIn = require("../middlewares/auth");
 
 router.use("/auth", authRouter);
-router.use("/user", userRouter);
-
+router.use("/user", isLoggedIn, userRouter);
 
 module.exports = router;
