2090871853a19097097b32a52e14171f7c3f710a
reza <rezarezaeijamiuk@gmail.com>
2022-06-10 18:10:00 +0100

    project refactored using startup folder

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
-require("express-async-errors");
 const express = require("express");
 const app = express();
 
-const mongoose = require("mongoose");
 const config = require("config");
 const debug = require("debug")("app:main");
-const winston = require("winston");
 
 const router = require("./src/routes");
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use(express.json());
+require("./startup/config")(app, express);
 
-mongoose
-  .connect(config.get("db.address"))
-  .then(() => debug("Connected to the mongoDB"))
-  .catch((err) => debug("Could not connect to the mongoDB!"));
-
-process.on("uncaughtException", (ex) => {
-  console.log("uncaught exception!");
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
-process.on("unhandledRejection", (ex) => {
-  console.log("unhandeled Rejection!");
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
-
-winston.add(new winston.transports.File({ filename: "errorsfile.log" }));
+require("./startup/db")();
+require("./startup/logging")();
 
 app.use("/api", router);
 
