8799c2615befd641cff1f450963f1616dac10c46
reza <rezarezaeijamiuk@gmail.com>
2022-06-11 12:06:59 +0100

    extra required package(bcrypt) removed

--- src/routes/admin/controller.js
+++ src/routes/admin/controller.js
@@ -1,6 +1,5 @@
 const controller = require("../controller");
 const _ = require("lodash");
-const bcrypt = require("bcrypt");
 
 module.exports = new (class extends controller {
   async dashboard(req, res) {
